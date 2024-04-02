40e4501f0bdefeefeef739c3f1776282c78b12e1
olnytim <yearagotomorrow@gmail.com>
2024-04-02 23:46:24 +0400

    preparing html site

--- srcs/sockets/WebServer.cpp
+++ srcs/sockets/WebServer.cpp
@@ -1,5 +1,5 @@
 #include "../../includes/sockets/WebServer.hpp"
-#include <string>
+#include <fstream>
 
 void WebServer::onClientConnected(int client) {
 }
@@ -18,3 +18,29 @@ void WebServer::onMessageReceived(int client, const char* msg, int length) {
              << "Hello World";
     sendToClient(client, response.str().c_str(), response.str().length() + 1);
 }
+
+//#include "../../includes/sockets/WebServer.hpp"
+//#include <fstream>
+//
+//void WebServer::onClientConnected(int client) {
+//}
+//
+//void WebServer::onClientDisconnected(int client) {
+//}
+//
+//void WebServer::onMessageReceived(int client, const char* msg, int length) {
+//    std::ifstream f("../../www/index.html");
+//    std::string output = "404 not found";
+//    if (f.good()) {
+//        std::ostringstream response;
+//        response << "HTTP/1.1 200 OK\r\n"
+//                 << "Cache-Control: no-cache, no-store, must-revalidate\r\n"
+//                 << "Content-Type: text/html\r\n"
+//                 << "Content-Length: 11\r\n"
+//                 << "Connection: close\r\n"
+//                 << "\r\n"
+//                 << "Hello World";
+//        output = response.str();
+//    }
+//    sendToClient(client, output.c_str(), output.length() + 1);
+//}
