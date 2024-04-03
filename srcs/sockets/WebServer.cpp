9423bfd55b854ff8fd1b60fdddffeb95ba612046
olnytim <yearagotomorrow@gmail.com>
2024-04-04 00:28:37 +0400

    so many things have  already done

--- srcs/sockets/WebServer.cpp
+++ srcs/sockets/WebServer.cpp
@@ -1,5 +1,7 @@
 #include "../../includes/sockets/WebServer.hpp"
 #include <fstream>
+#include <vector>
+#include <string>
 
 void WebServer::onClientConnected(int client) {
 }
@@ -8,39 +10,37 @@ void WebServer::onClientDisconnected(int client) {
 }
 
 void WebServer::onMessageReceived(int client, const char* msg, int length) {
+    std::istringstream iss(msg);
+    std::vector<std::string> tokens;
+    std::string token;
+    while (std::getline(iss, token, ' ')) {
+        tokens.push_back(token);
+    }
+    std::string content = "<h1>404 not found</h1>";
+    std::string fileHtml = "index.html";
+    int error_code = 404;
+
+    if (tokens.size() >= 3 && tokens[0] == "GET") {
+        fileHtml = "../www/" + tokens[1];
+    }
+
+    std::ifstream f(fileHtml.c_str());
+    if (f.good()) {
+        error_code = 200;
+        std::stringstream buffer;
+        buffer << f.rdbuf();
+        content = buffer.str();
+    }
+    f.close();
     std::ostringstream response;
-    response << "HTTP/1.1 200 OK\r\n"
+    std::cout << content.size() << std::endl;
+    response << "HTTP/1.1 " << error_code << " OK\r\n"
              << "Cache-Control: no-cache, no-store, must-revalidate\r\n"
              << "Content-Type: text/html\r\n"
-             << "Content-Length: 11\r\n"
+             << "Content-Length: " << content.size() << "\r\n"
              << "Connection: close\r\n"
              << "\r\n"
-             << "Hello World";
-    sendToClient(client, response.str().c_str(), response.str().length() + 1);
+             << content;
+    std::string output = response.str();
+    sendToClient(client, output.c_str(), output.length() + 1);
 }
-
-//#include "../../includes/sockets/WebServer.hpp"
-//#include <fstream>
-//
-//void WebServer::onClientConnected(int client) {
-//}
-//
-//void WebServer::onClientDisconnected(int client) {
-//}
-//
-//void WebServer::onMessageReceived(int client, const char* msg, int length) {
-//    std::ifstream f("../../www/index.html");
-//    std::string output = "404 not found";
-//    if (f.good()) {
-//        std::ostringstream response;
-//        response << "HTTP/1.1 200 OK\r\n"
-//                 << "Cache-Control: no-cache, no-store, must-revalidate\r\n"
-//                 << "Content-Type: text/html\r\n"
-//                 << "Content-Length: 11\r\n"
-//                 << "Connection: close\r\n"
-//                 << "\r\n"
-//                 << "Hello World";
-//        output = response.str();
-//    }
-//    sendToClient(client, output.c_str(), output.length() + 1);
-//}
