3263dd29320be6e2c8fa7a863d0da9a51488c1d2
olnytim <yearagotomorrow@gmail.com>
2024-04-04 18:27:57 +0400

    jpeg already works

--- srcs/sockets/WebServer.cpp
+++ srcs/sockets/WebServer.cpp
@@ -18,12 +18,12 @@ void WebServer::onMessageReceived(int client, const char* msg, int length) {
     }
     std::string content = "<h1>404 not found</h1>";
     std::string fileHtml = "index.html";
+    std::string contentType;
     int error_code = 404;
 
     if (tokens.size() >= 3 && tokens[0] == "GET") {
         fileHtml = "../www/" + tokens[1];
     }
-
     std::ifstream f(fileHtml.c_str());
     if (f.good()) {
         error_code = 200;
@@ -31,12 +31,26 @@ void WebServer::onMessageReceived(int client, const char* msg, int length) {
         buffer << f.rdbuf();
         content = buffer.str();
     }
+    if (fileHtml.find(".html") != std::string::npos) {
+        contentType = "html";
+    } else if (fileHtml.find(".css") != std::string::npos) {
+        contentType = "css";
+    } else if (fileHtml.find(".jpeg") != std::string::npos) {
+        contentType = "jpeg";
+        std::fstream f(fileHtml.c_str(), std::ios::in | std::ios::binary);
+        if (f.good()) {
+            f.seekg(0, std::ios::end);
+            content.resize(f.tellg());
+            f.seekg(0, std::ios::beg);
+            f.read(&content[0], content.size());
+        }
+    }
     f.close();
     std::ostringstream response;
     std::cout << content.size() << std::endl;
     response << "HTTP/1.1 " << error_code << " OK\r\n"
              << "Cache-Control: no-cache, no-store, must-revalidate\r\n"
-             << "Content-Type: text/html\r\n"
+             << "Content-Type: text/" << contentType << "\r\n"
              << "Content-Length: " << content.size() << "\r\n"
              << "Connection: close\r\n"
              << "\r\n"
